4a8889ef0a1e486862183b517e09808d4acbed83
Damjes <git@damj.es>
2025-12-27 08:35:21 +0100

    Dostrojenie ESLinta, który panikuje, jak zobaczy refa

--- src/komponenty/Okienko/Okienko.tsx
+++ src/komponenty/Okienko/Okienko.tsx
@@ -6,6 +6,10 @@ function Okienko(props: {
 	tekst: string
 	refOkienka: React.RefObject<HTMLDialogElement | null>
 }) {
+	/* eslint-disable react-hooks/refs */
+	// bez tego eslint piłuje mordę, że rzekomo korzystam z propsów przy renderowaniu komponentu, ale to wreżala
+	// korzystam raz do ustawienia refa, ale NB https://react.dev/learn/manipulating-the-dom-with-refs#accessing-another-components-dom-nodes
+
 	function zamknij() {
 		props.refOkienka.current?.close()
 	}
