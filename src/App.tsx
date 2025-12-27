5a3d2e85245de1ed15ef30579774ebcb354bbe0d
Damjes <git@damj.es>
2025-12-27 06:10:10 +0100

    Switch dark corner to steelblue

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import GithubCorner from './komponenty/GithubCorner/GithubCorner'
 import Gra from './komponenty/Gra/Gra'
 
 function App() {
 	return (
 		<>
+			<GithubCorner
+				href="https://github.com/damjes/wordle/"
+				ariaLabel="Legu fontkodon en GitHub"
+			/>
 			<h1>Damjesowe Wordle</h1>
 			<Gra />
 		</>
